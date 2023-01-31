b94f7b4ac10f80cfc47b25635c455d87f4dd2554
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-01-31 19:28:01 +0600

    added html and javascript file

---
+++ script.js
@@ -0,0 +1,24 @@
+const quizData = [
+    {
+        question: 'Independent day of Bangladesh?',
+        a: '1st April',
+        b: '26th March',
+        c: '16th December',
+        d: '1st June',
+        corrcet: 'b'
+    },{
+        question: 'What does WWW means?',
+        a: 'World Wide Web',
+        b: 'World Web Wide',
+        b: 'Web Wide World',
+        b: 'Web World Wide',
+        corrcet: 'a'
+    },{
+        question: 'Which one is not a programming language?',
+        a: 'Java',
+        b: 'C++',
+        c: 'HTML',
+        d: 'Python',
+        corrcet: 'c'
+    }
+];
